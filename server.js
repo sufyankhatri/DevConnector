dd7ae445ca0eabd71b012d9d74368ab4b031c821
sufyankhatri <43412206+sufyankhatri@users.noreply.github.com>
2019-07-17 14:12:52 +0500

    Initial commit

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const connectDB= require('./config/db');
 const app = express();
 
+
+connectDB();
 app.get('/', (req, res) => res.send('API running'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
