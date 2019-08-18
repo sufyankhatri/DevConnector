275aa03daf26c43282e4e2a6b3abe4d3ed8145d0
sufyankhatri <sufyankhatree@gmail.com>
2019-08-18 22:32:15 +0500

    prepare to deploy

---
+++ client/build/precache-manifest.2186ea0f7515ae58b7ef2e05dca7cf8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc17ced1ebd5ae1fbeaa2c8d96bd6b7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd904705f876ab2b6810",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "299cb18eb708538c0418",
+    "url": "/static/js/2.40cf7465.chunk.js"
+  },
+  {
+    "revision": "bd904705f876ab2b6810",
+    "url": "/static/js/main.d777ae09.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
--- server.js
+++ server.js
@@ -13,5 +13,14 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
